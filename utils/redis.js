fd89761db1f791258a8c60ee98fef33013bb13e0
Patrick Mutabazi <patrickmtbz@gmail.com>
2024-12-09 21:27:28 +0300

    Redis utils

--- utils/redis.js
+++ utils/redis.js
@@ -1,26 +1,76 @@
-reuire('dotenv').config();
+require('dotenv').config();  // Retrieve variables from .env file
 const { createClient } = require('redis');
 
 
+// RedisClient class
 class RedisClient {
-  constructor(redisClient) {
+  constructor() {
     try {
     this.redisClient = createClient({
       socket: {
         host: process.env.REDIS_HOST,
         port: process.env.REDIS_PORT
-      }
+      },
     });
-    } on('error') {
+    
+    // Handle connection errors
+    this.redisClient.on('error', (err) => {
       console.error('Could not connect to Redis:', err);
+    });
+    } catch(err) {
+      console.error('Error initializing Redis client:', err);
     }
   }
 
-  async isAlive() => {
+  // Check if connection to Redis is successfull
+  async isAlive() {
   try {
-    await this.redisClient.connect
+    await this.redisClient.connect();
+    console.log('Connected to Redis');
+    return true;
   } catch (err) {
-    
+    console.error('Could not connect to Redis:', err);
+    return false;
   }
+  }
+
+  // Get value for key stored in Redis
+  async get(key) {
+    try {
+      const value = await this.redisClient.get(key);
+    
+      if (value === null) {
+        console.error('Key not found or expired');
+        return null;
+      }
+      return value;
+      } catch (err) {
+        console.error('Error getting key:', err);
+        return null;
+      }
+    }
+  
+  // Store a key, value and  duration (in seconds) in Redis
+  async set(key, value, ttl) {
+    try { 
+      await this.redisClient.setEx(key, ttl, value);
+    } catch (err) {
+      console.error('Error getting key:', err);
+    }
+  }
+
+  // Remove a key stored in Redis
+  async del(key) {
+    try {
+    await this.redisClient.del(key);
+    } catch (err) {
+       console.error('Error getting key:', err)
+    }
   }
 }
+
+
+// Creating and exporting an instance of RedisClient
+const redisClient = new RedisClient();
+
+module.exports = redisClient;
