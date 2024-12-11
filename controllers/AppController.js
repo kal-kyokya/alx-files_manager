ef45e2712d589114f6de5536a8b023e4c139c7e5
MutabPato <patrickmtbz@gmail.com>
2024-12-11 12:31:39 -0800

    Minor syntax fixes

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,22 +2,29 @@
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
-
 class AppController {
- // Get status of Redis and DB
- static getStatus(req, res) {
-   const status = {
-     redis: redisClient.isAlive(),
-     db: dbClient.isAlive(),
-   };
-    res.status(200).send(status);
-  } 
+  /**
+   * Get status of Redis and DB
+   * @param {Object} req - The request object
+   * @param {Object} res - The response object
+   */
+  static getStatus(req, res) {
+    const status = {
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    };
+    res.status(200).send(status); // Send the status as a JSON response
+  }
 
-  // Get number of users and files in DB
+  /**
+   * Get number of users and files in DB
+   * @param {Object} req - The request object
+   * @param {Object} res - The response object
+   */
   static async getStats(req, res) {
     const stats = {
-      users : await dbClient.nbUsers(),
-      files : await dbClient.nbFiles(),
+      users: await dbClient.nbUsers(),
+      files: await dbClient.nbFiles(),
     };
     res.status(200).send(stats);
   }
