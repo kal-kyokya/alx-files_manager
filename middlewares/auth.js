da81ec2965c637d6e93dbfdb7d6b55198dbdf83b
kal-kyokya <kalkyokya4@gmail.com>
2024-12-16 11:18:44 -0800

    Debug 'auth middleware' script and resolve ESLint.

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,26 +3,26 @@
 import { getUserFromXToken, getUserFromAuthorization } from '../utils/auth';
 
 // Function applying Basic Authentication to a route
-export const basicAuth = async (res, res, next) {
-    const user = await getUserFromAuthorization(req);
+export const basicAuth = async (req, res, next) => {
+  const user = await getUserFromAuthorization(req);
 
-    if (!user) {
-	res.status(401).json({ error: 'Unauthorized' });
-	return;
-    }
+  if (!user) {
+    res.status(401).json({ error: 'Unauthorized' });
+    return;
+  }
 
-    req.user = user;
-    next();
+  req.user = user;
+  next();
 };
 
 // Function applying Token-Based Authentication to a route
 export const xTokenAuth = async (req, res, next) => {
-    const user = await getUserFromXtoken(req);
+  const user = await getUserFromXToken(req);
 
-    if (!user) {
-	res.status(401).json({ error: 'Unauthorized' });
-    }
+  if (!user) {
+    res.status(401).json({ error: 'Unauthorized' });
+  }
 
-    req.user = user;
-    next();
+  req.user = user;
+  next();
 };
