3ff9237031712c49c3e17f33411646a11a1e9153
kal-kyokya <kalkyokya4@gmail.com>
2025-01-07 03:41:58 -0800

    Investigate redis load error.

--- utils/auth.js
+++ utils/auth.js
@@ -2,8 +2,8 @@
 
 // eslint-disable import/no-named-as-default
 // eslint-disable no-unused-vars
+import { ObjectId } from 'mongodb';
 import sha1 from 'sha1';
-import mongoDBCore from 'mongodb/lib/core';
 import { dbClient } from './db';
 import redisClient from './redis';
 
@@ -44,8 +44,7 @@ export const getUserFromXToken = async (req) => {
   if (!userId) {
     return null;
   }
-  const user = await (await dbClient.usersCollection())
-    .findOne({ _id: new mongoDBCore.BSON.ObjectId(userId) });
+  const user = await (await dbClient.usersCollection()).findOne({ _id: ObjectId(userId) });
 
   return user || null;
 };
